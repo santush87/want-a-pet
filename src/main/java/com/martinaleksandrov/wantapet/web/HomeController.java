ddee73b398adc7708ee4071071174e61b4bb7735
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-09-25 14:08:59 +0300

    Added HomeController

--- src/main/java/com/martinaleksandrov/wantapet/web/HomeController.java
+++ src/main/java/com/martinaleksandrov/wantapet/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.martinaleksandrov.wantapet.controllers;
+package com.martinaleksandrov.wantapet.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
