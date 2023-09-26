a04b9ad2b7f47377ddd8102f9d7472c672897ebf
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-09-26 14:01:39 +0300

    Created base entity

---
+++ src/main/java/com/martinaleksandrov/wantapet/models/entities/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.martinaleksandrov.wantapet.models.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid-string")
+    @GenericGenerator(name = "uuid-string",
+            strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+}
