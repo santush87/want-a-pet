c656084d71ec82c9cfe3129802973d0f9ec934d6
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-09-26 14:22:26 +0300

    Added ModdelMapper Bean

--- src/main/java/com/martinaleksandrov/wantapet/config/AppConfig.java
+++ src/main/java/com/martinaleksandrov/wantapet/config/AppConfig.java
@@ -1,15 +1,29 @@
 package com.martinaleksandrov.wantapet.config;
 
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    public ModelMapper modelMapper(){
+        final ModelMapper mapper = new ModelMapper();
+
+        mapper.addConverter(new Converter<String, LocalDate>() {
+            @Override
+            public LocalDate convert(MappingContext<String, LocalDate> context) {
+                return LocalDate.parse(context.getSource(),
+                        DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            }
+        });
+        return mapper;
     }
+
 }
