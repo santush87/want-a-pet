66b94a5e0a4f3442c0ad41afca14fb6e6ce605ce
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-10-03 18:05:46 +0300

    Added registerDto type

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -5,10 +5,10 @@ import { Routes, Route } from 'react-router-dom'
 import * as petServices from './services/petService'
 import Header from './components/Header/Header';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import RegisterPage from './components/models/RegisterPage';
 import Home from './components/Home/Home';
 import { useEffect, useState } from 'react';
 import { Pet } from './lib/types';
+import UserRegister from './components/UserRegister/UserRegister';
 
 
 function App() {
@@ -27,7 +27,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				{/* <Route path='/login' element={<RegisterPage />} /> */}
-				<Route path='/register' element={<RegisterPage />} />
+				<Route path='/register' element={<UserRegister />} />
 				{/* <Route path='/add-pet' element={<RegisterPage />} />
         <Route path='/catalog' element={<RegisterPage />} />
         <Route path='/contact' element={<RegisterPage />} /> */}
--- src/main/frontend/src/lib/types.ts
+++ src/main/frontend/src/lib/types.ts
@@ -10,3 +10,25 @@ export type User = {
   firstName: string;
   lastName: string;
 };
+
+export type UserRegisterDto = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  type: "PRIVATE" | "BUSINESS";
+  address: Address;
+};
+
+export type Address = {
+  country:
+    | "BULGARIA"
+    | "ROMANIA"
+    | "GREECE"
+    | "GERMANY"
+    | "SPAIN"
+    | "SWITZERLAND";
+  city: string;
+  street: string;
+  number: number;
+};
