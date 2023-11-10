3d470e990765f7b4427e0e70e2bc12e2f43bc6c5
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-11-10 13:23:35 +0200

    Successful registration test!

---
+++ src/test/java/com/martinaleksandrov/wantapet/web/AuthControllerTestIT.java
@@ -0,0 +1,44 @@
+package com.martinaleksandrov.wantapet.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTestIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+//    @BeforeEach
+//    void setUp() {
+//    }
+
+    @Test
+    void testRegistration() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/register")
+                        .param("firstName", "Martin")
+                        .param("lastName", "Aleksandrov")
+                        .param("email", "martin@abv.bg")
+                        .param("password", "asdasdasd")
+                        .param("confirmPassword", "asdasdasd")
+                        .param("phoneNumber", "0877123456" )
+                        .param("country", "BULGARIA")
+                        .param("city", "Sofia")
+                        .param("street", "Ivan Vazov")
+                        .param("streetNumber", "25")
+                        .param("userType", "PRIVATE")
+                        .with(csrf())
+        ).andExpect(status().is3xxRedirection());
+    }
+}
