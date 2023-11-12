e08eef68fe6ef94628e1602236cb9110d6086173
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-11-12 13:08:15 +0200

    Added new annotation for Password Matching

--- src/main/java/com/martinaleksandrov/wantapet/models/dtos/UserRegisterDto.java
+++ src/main/java/com/martinaleksandrov/wantapet/models/dtos/UserRegisterDto.java
@@ -1,5 +1,6 @@
 package com.martinaleksandrov.wantapet.models.dtos;
 
+import com.martinaleksandrov.wantapet.annotation.PasswordMatch;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
@@ -13,44 +14,44 @@ import org.springframework.format.annotation.NumberFormat;
 @Getter
 @Setter
 @NoArgsConstructor
-@SuperBuilder
+@PasswordMatch
 public class UserRegisterDto {
 
     @Email
     @NotBlank(message = "Email cannot be empty!")
     private String email;
 
-//    @NotBlank
+    //    @NotBlank
     @Size(min = 2, max = 20, message = "First name must be between 2 and 20 characters!")
     private String firstName;
 
-//    @NotBlank
+    //    @NotBlank
     @Size(min = 2, max = 20, message = "Last name must be between 2 and 20 characters!")
     private String lastName;
 
-//    @NotBlank
+    //    @NotBlank
     @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters!")
     private String password;
 
+//    @PasswordMatch
     private String confirmPassword;
 
     @NotBlank(message = "Select a user type!")
     private String userType;
 
-//    @NotBlank
+    //    @NotBlank
     @Size(min = 8, max = 20, message = "Phone number must be between 8 and 20 characters!")
     private String phoneNumber;
 
     @NotBlank(message = "Select a country!")
     private String country;
 
-//    @NotBlank
+    //    @NotBlank
     @Size(min = 2, message = "City length must be at least 2 characters!")
     private String city;
 
     @NotBlank(message = "Enter a street!")
     private String street;
 
-//    @Positive(message = "Number should be a positive number")
-    private int streetNumber;
+    private String streetNumber;
 }
