f67a7725fa1854a4c64a2ce293f2ad88008ba7d5
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-11-12 13:09:11 +0200

    For image uploading

---
+++ src/main/java/com/martinaleksandrov/wantapet/models/entities/Image.java
@@ -0,0 +1,20 @@
+package com.martinaleksandrov.wantapet.models.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "images")
+@Getter
+@Setter
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private byte[] data;
+
+}
---
+++ src/main/java/com/martinaleksandrov/wantapet/services/ImageService.java
@@ -0,0 +1,21 @@
+package com.martinaleksandrov.wantapet.services;
+
+import com.martinaleksandrov.wantapet.models.entities.Image;
+import com.martinaleksandrov.wantapet.reporitories.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    public void saveImage(Image image) {
+        imageRepository.save(image);
+    }
+
+    public Image getImageById(Long id) {
+        return imageRepository.findById(id).orElse(null);
+    }
+}
