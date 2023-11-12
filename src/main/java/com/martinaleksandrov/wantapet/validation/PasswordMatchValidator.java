1f75dc58093368d7ef7c97aaa51cd5ad06550118
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-11-12 13:07:46 +0200

    Added new annotation for Password Matching

---
+++ src/main/java/com/martinaleksandrov/wantapet/validation/PasswordMatchValidator.java
@@ -0,0 +1,46 @@
+package com.martinaleksandrov.wantapet.validation;
+
+import com.martinaleksandrov.wantapet.annotation.PasswordMatch;
+import com.martinaleksandrov.wantapet.models.dtos.UserRegisterDto;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
+
+public class PasswordMatchValidator implements ConstraintValidator<PasswordMatch, UserRegisterDto> {
+    private String message;
+
+    @Override
+    public void initialize (PasswordMatch constraintAnnotation) {
+
+        this.message = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid (UserRegisterDto userRegisterDto, ConstraintValidatorContext context) {
+
+        final String password = userRegisterDto.getPassword();
+        final String confirmPassword = userRegisterDto.getConfirmPassword();
+
+        if (password == null && confirmPassword == null) {
+
+            return true;
+        } else {
+
+            boolean passwordMatch = password != null && password.equals(confirmPassword);
+
+            if (!passwordMatch) {
+
+                HibernateConstraintValidatorContext hibernateContext =
+                        context.unwrap(HibernateConstraintValidatorContext.class);
+
+                hibernateContext
+                        .buildConstraintViolationWithTemplate(message)
+                        .addPropertyNode("confirmPassword")
+                        .addConstraintViolation()
+                        .disableDefaultConstraintViolation();
+            }
+
+            return passwordMatch;
+        }
+    }
+}
