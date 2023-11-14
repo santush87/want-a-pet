5120e89dbff6ca7f10e0a3760d4e4b7f691e272b
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-11-14 18:35:53 +0200

    Code refactoring!

--- src/main/java/com/martinaleksandrov/wantapet/services/PetService.java
+++ src/main/java/com/martinaleksandrov/wantapet/services/PetService.java
@@ -2,11 +2,18 @@ package com.martinaleksandrov.wantapet.services;
 
 import com.martinaleksandrov.wantapet.models.dtos.PetCreatingDto;
 import com.martinaleksandrov.wantapet.models.entities.PetEntity;
+import com.martinaleksandrov.wantapet.models.entities.UserEntity;
 import com.martinaleksandrov.wantapet.models.enums.PetType;
 import com.martinaleksandrov.wantapet.reporitories.PetRepository;
+import com.martinaleksandrov.wantapet.reporitories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,20 +21,27 @@ public class PetService {
 
     private final PetRepository petRepository;
     private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
 
     public void addDog(PetCreatingDto petCreatingDto) {
-        PetEntity pet = addPet(petCreatingDto);
-        pet.setType(PetType.DOG);
-        this.petRepository.save(pet);
+        addPet(petCreatingDto, PetType.DOG);
     }
 
     public void addCat(PetCreatingDto petCreatingDto) {
-        PetEntity pet = addPet(petCreatingDto);
-        pet.setType(PetType.CAT);
-        this.petRepository.save(pet);
+        addPet(petCreatingDto, PetType.CAT);
     }
 
-    private PetEntity addPet(PetCreatingDto petCreatingDto) {
-        return this.modelMapper.map(petCreatingDto, PetEntity.class);
+    private void addPet(PetCreatingDto petCreatingDto, PetType petType) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        UserEntity user = this.userRepository.findByEmail(userDetails.getUsername()).get();
+
+        PetEntity pet = this.modelMapper.map(petCreatingDto, PetEntity.class);
+
+        pet.setType(petType);
+        this.petRepository.save(pet);
+
+        user.getPets().add(pet);
+        this.userRepository.save(user);
     }
 }
