eb92accb2910ae22fe6d8fe24cf0901aeabcd65c
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-11-24 13:27:30 +0200

    Added more readable names

--- src/main/java/com/martinaleksandrov/wantapet/models/enums/WeightRange.java
+++ src/main/java/com/martinaleksandrov/wantapet/models/enums/WeightRange.java
@@ -1,8 +1,18 @@
 package com.martinaleksandrov.wantapet.models.enums;
 
 public enum WeightRange {
-    BETWEEN_0KG_AND_5KG,
-    BETWEEN_6KG_AND_10KG,
-    BETWEEN_11KG_AND_25KG,
-    UP_25KG
+    BETWEEN_0KG_AND_5KG("Between 0kg and 5kg"),
+    BETWEEN_6KG_AND_10KG("Between 6kg and 10kg"),
+    BETWEEN_11KG_AND_25KG("Between 11kg and 25kg"),
+    UP_25KG("More than 25kg");
+
+    public final String name;
+
+    private WeightRange(String weight) {
+        this.name = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
