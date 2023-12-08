ce80c6e9161795b26e5766e287e081a4d4671ddd
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-12-08 15:54:13 +0200

    Added new custom exception

---
+++ src/main/java/com/martinaleksandrov/wantapet/exceptions/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.martinaleksandrov.wantapet.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable ex) {
+        super(message, ex);
+    }
+}
